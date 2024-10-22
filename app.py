9e7255b3f9fd7e00921c9e9c0c3e0122297baf69
Thomas Bender <thobep21@hotmail.com>
2024-10-22 19:20:05 -0300

    passando a variavel opts para o webdriver

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ from selenium.webdriver import ChromeOptions
 opts = ChromeOptions()
 opts.add_experimental_option("detach", True)
 
+#passe a variavel opts para o webdriver
+
 navegador = webdriver.Chrome(options = opts)
+url = r"https://www.duque.g12.br"
+navegador.get(url)
