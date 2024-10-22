c84fe6da3b00522ddbe6beb40de108b309cefdcd
Thomas Bender <thobep21@hotmail.com>
2024-10-22 19:29:30 -0300

    criando uma pagina web para testes

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ opts.add_experimental_option("detach", True)
 #passe a variavel opts para o webdriver
 
 navegador = webdriver.Chrome(options = opts)
-url = r"https://www.duque.g12.br"
+url = r"C:\Users\04396\Desktop\projetos\webscrap\produtos.html"
 navegador.get(url)
+print(navegador.title)
