343f41ba2131a3ab979702e43396c5afa704b98f
Thomas Bender <thobep21@hotmail.com>
2024-10-22 20:52:58 -0300

    criando mais uma pagina de teste web

--- app.py
+++ app.py
@@ -11,13 +11,18 @@ navegador = webdriver.Chrome(options = opts)
 
 url = r"C:\Users\04396\Desktop\projetos\webscrap\produtos.html"
 navegador.get(url)
+
 print(navegador.title)
 
 titulo = navegador.find_element(By.ID, "titulo")
 precos = navegador.find_elements(By.CLASS_NAME, "price")
+
 for preco in precos:
     print(preco.text)
 produtos = navegador.find_elements(By.CLASS_NAME, "product_name")
 
 for produto in produtos:
     print(produto.text)
+
+precos = navegador.find_elements(By.XPATH, "/html/body/div/div[1]/p")
+print(preco.text)
