14aed2ac338a37da117353f0d09f21a6ffc211c5
Thomas Bender <thobep21@hotmail.com>
2024-10-22 20:00:24 -0300

    testes

--- app.py
+++ app.py
@@ -8,10 +8,16 @@ opts.add_experimental_option("detach", True)
 #passe a variavel opts para o webdriver
 
 navegador = webdriver.Chrome(options = opts)
+
 url = r"C:\Users\04396\Desktop\projetos\webscrap\produtos.html"
 navegador.get(url)
 print(navegador.title)
 
 titulo = navegador.find_element(By.ID, "titulo")
 precos = navegador.find_elements(By.CLASS_NAME, "price")
-print(titulo)
+for preco in precos:
+    print(preco.text)
+produtos = navegador.find_elements(By.CLASS_NAME, "product_name")
+
+for produto in produtos:
+    print(produto.text)
